c754001729ea73cbd273d2fe594e7e64c694513f
Eduardo Barbosa Bernardes <182210134@senacinformatica.br>
2023-09-15 10:01:21 -0300

    Primeiro commit na minha tela

--- src/mensagemdodia/MensagemDoDia.java
+++ src/mensagemdodia/MensagemDoDia.java
@@ -1,21 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package mensagemdodia;
 
-/**
- *
- * @author 182210120
- */
-public class MensagemDoDia {
+import javax.swing.JFrame;
+import view.Janela;
 
-    /**
-     * @param args the command line arguments
-     */
+public class MensagemDoDia {
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Janela jan = new Janela();
+        
+        jan.setVisible(true);
+        jan.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
     }
     
 }
